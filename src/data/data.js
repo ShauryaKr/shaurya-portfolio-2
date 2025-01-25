37b69a96faf413a88786334d47697922eafa9c0e
ShauryaKr <shauryakumar700@gmail.com>
2025-01-25 22:31:56 +0530

    data.js

--- src/data/data.js
+++ src/data/data.js
@@ -20,10 +20,18 @@ export const data = [
   {
     id: 3,
     img: 'https://i.postimg.cc/y6tnhqWX/raspberry-pi-drone-768x576.jpg',
-    title: "Autonomous Drone",
-    desc: "Developed it as a college project using Raspberry Pi and Pixhawk flight controller",
+    title: "Quad-copter | Drone #1",
+    desc: "Developed it as a college project using Pixhawk, Telemerty & MAV-link",
     link: "https://www.instagram.com/reel/CcDASP2lfDd/?utm",
     git: "https://www.instagram.com/reel/CcDASP2lfDd/?utm",
+  },
+  {
+    id: 4,
+    img: 'https://i.postimg.cc/L6qM4JZJ/hexacopter.jpg',
+    title: "Hexa-copter | Drone #2",
+    desc: "Built a drone using pixhawk, raspberry pi, google coral tpu, etc.",
+    link: "https://photos.app.goo.gl/G2TFYLQbkTwdHEn49",
+    git: "https://photos.app.goo.gl/G2TFYLQbkTwdHEn49",
   }
   
 ];
